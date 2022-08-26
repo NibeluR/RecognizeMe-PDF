e7904882cb8b233198067b0ec46ad2a9a352a8f7
BuildTools <unconfigured@null.spigotmc.org>
2022-08-26 17:12:22 +0300

    fix-3d

--- app.js
+++ app.js
@@ -63,3 +63,108 @@ app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
+
+
+
+/* //TESTING CODE FOLLOW MOUSE
+
+const canvas = document.querySelector('canvas.webgl');
+
+const PI = Math.PI;
+
+const scene = new THREE.Scene();
+
+// INSTANTIATE LOADER
+const loader = new THREE.GLTFLoader();
+
+const modelGroup = new THREE.Group();
+
+// LOAD GLTF MODEL
+loader.load(
+  '/images/profile-icon.glb',
+  function(gltf) {
+
+    modelGroup.add(gltf.scene);
+    scene.add(modelGroup);
+
+  },
+  // IN PROGRESS
+  function(xhr) {
+
+    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+
+  },
+  // ERROR
+  function(error) {
+
+    console.log('An error happened');
+
+  }
+);
+
+// WINDOW SIZE OBJECT
+const sizes = {
+  width: window.innerWidth / 2.5,
+  height: window.innerHeight / 2.5
+};
+
+// INSTANTIATE CAMERA
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 23;
+scene.add(camera);
+
+// LIGHTING
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.2);
+directionalLight.position.x = 20;
+directionalLight.position.z = 10;
+scene.add(directionalLight);
+
+// WINDOW RESIZE HANDLER
+window.addEventListener('resize', () => {
+  sizes.width = window.innerWidth / 2.5;
+  sizes.height = window.innerHeight / 2.5;
+
+  camera.aspect = (sizes.width / sizes.height);
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(sizes.width, sizes.height);
+});
+
+// MOUSEMOVE ANIMATION
+window.addEventListener('mousemove', (event) => {
+  modelGroup.rotation.y = (event.clientX / window.innerWidth) - 0.5;
+  modelGroup.rotation.x = (event.clientY / window.innerHeight) - 0.5;
+
+  modelGroup.position.x = ((event.clientX / window.innerWidth) - 0.5) * 15;
+  modelGroup.position.y = ((event.clientY / window.innerHeight) - 0.5) * -15;
+});
+
+// INSTANTIATE RENDERER
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  alpha: true
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+renderer.setClearColor(0x000000, 0);
+
+// ANIMATION LOOP
+const tick = () => {
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(tick);
+};
+tick();
+
+
+window.addEventListener('mousemove', function(e){
+    var mouse3D = new THREE.Vector3(
+        ( event.clientX / window.innerWidth ) * 2 - 1,
+        - ( event.clientY / window.innerHeight ) * 2 + 1,
+        0.5 );
+
+    your3Dobject.lookAt(mouse3D);
+}) */
